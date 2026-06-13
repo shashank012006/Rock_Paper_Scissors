984663569b6194cf5c66f048856a174337a3788b
shashank012006 <shashankv8432@gmail.com>
2026-06-13 23:37:31 +0530

    combined game logics with the doms

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("Hello player ! ");
 
-
+const rockButton = document.getElementById("rockBtn");
+const paperButton = document.getElementById("paperBtn");
+const scissorsButton = document.getElementById("scissorsBtn");
 
 let humanScore = 0;
 let computerScore = 0;
@@ -11,19 +13,22 @@ function getCC(){
     return items[randomc]
 }
 
-function getHC(){
-    // userInput = prompt("Enter rock,paper and scissors");
-    // playc = userInput ? userInput.trim().toLowerCase() : "";
-    // return playc
-    const rockButton = document.getElementById("myButton");
-    
-    rockButton.addEventListener("click", () => {
-        const choice = "rock";
-        return choice
-    });
-}
+
+let minRound = 0;
+const maxRound = 5;
 
 function play(humanc,computerc){
+    
+
+    if (minRound>=maxRound){
+        console.log("U have finished game ! ");
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        return;
+    }
+
+    minRound++
     
     if (humanc === computerc){
         console.log("it is tie");
@@ -31,50 +36,50 @@ function play(humanc,computerc){
     else if (humanc === "paper" && computerc === "rock"){
         console.log("player won");
         humanScore += 1;
+    
     }
     else if (humanc === "rock" && computerc === "paper"){
         console.log("computer won");
         computerScore += 1;
+    
     }
     else if (humanc === "rock" && computerc === "scissors"){
         console.log("player won");
         humanScore += 1;
+    
     }
     else if (humanc === "scissors" && computerc === "rock"){
         console.log("computer won");
         computerScore += 1;
+    
     }
     else if (humanc === "scissors" && computerc === "paper"){
         console.log("player won");
         humanScore += 1;
+    
     }
     else if (humanc === "paper" && computerc === "scissors"){
         console.log("computer won");
         computerScore += 1;
+    
     }
     else{
         console.log("Invalid");
+    
     }
+}
     
+const computerc = getCC();
+rockButton.addEventListener("click", () => {
+    play("rock",computerc);
+});
     
-}
+paperButton.addEventListener("click", () => {
+    play("paper",computerc);
+});
+    
+scissorsButton.addEventListener("click", () => {
+    play("scissors",computerc);
+});
 
-for (i=0;i<=0;i++){
-    const humanc = getHC();
-    const computerc = getCC();
-    play(humanc,computerc);
-}
 
-console.log("Total score for each : \n");
-console.log("Player : " + humanScore + "  computer : " + computerScore);
-
-if (humanScore == computerScore){
-    console.log("Nice try Champ");
-}
-else if (humanScore > computerScore){
-    console.log("Player is the final winner!");
-}
-else if (humanScore < computerScore){
-    console.log("computer is the final winner!");
-
-}
